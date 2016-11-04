792f0015451ce0934a1329d841696275c02dbff8
jakezappin <jake.zappin@gmail.com>
2016-11-04 15:00:11 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,30 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('hey!')
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(element) {
+    // 'g' corresponds to 71
+    if (element.which === 71) {
+      alert("You pressed 'g'!")
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function(element) {
+    alert('Your form is going to be submitted now.')
+  })
+}
+
 
 $(document).ready(function(){
 
